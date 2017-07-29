34420eaa74b77f4e878ca738123c465a3c67aed4
enlyte <moyboy@live.com>
2017-07-29 19:07:14 -0400

    Initial Commit
    Added files to Folder

---
+++ js/practice.js
@@ -0,0 +1,5 @@
+var josh = 12;
+var steve = 34;
+var addAges = josh + steve;
+
+document.getElementById('age-container').innerHTML = "Joshes age = " + josh + " Steve's age is " + steve + " Our combined ages are " + addAges;
